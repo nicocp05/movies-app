a2822aedcd946748d386825899e0cf74a4ef9336
Nicolas <nico_cp05@hotmail.com>
2021-04-13 20:58:33 -0300

    Add env.port in server.js

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/movies-app'))
+app.use(express.static(__dirname + '/dist/movies-app'));
+
+app.set('port', process.env.PORT || 3000);
 
 app.get('/home', (req, res) => {
     res.sendFile(path.join(__dirname + '/dist/movies-app/index.html'));
 });
 
-app.listen(3000, () => {
-    console.log('Server on Port 3000');
+app.listen(app.get('port'), () => {
+    console.log('Server on Port ' + app.get('port'));
 });
